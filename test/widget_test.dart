317354b655e2d01ed7e5617474d939bbdec90cb0
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-11-28 21:12:04 +0800

    Test, I got smoke

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wifilogin/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('I got smoke', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.byIcon(Icons.home), findsOneWidget);
+    expect(find.byIcon(Icons.person), findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
   });
 }
