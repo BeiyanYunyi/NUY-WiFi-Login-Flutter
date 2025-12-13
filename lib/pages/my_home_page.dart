e98dec13400c2e997a6aa3811d15a5bae805b1b3
Midori Kochiya <i@reimu.moe>
2025-12-13 19:22:08 +0800

    fix ci

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -23,7 +23,7 @@ class MyHomePage extends StatelessWidget {
           Obx(() => c.ip.value != ''
               ? Text(
                   '你的 IP：${c.ip}',
-                  style: Theme.of(context).textTheme.headline4,
+                  style: Theme.of(context).textTheme.headlineMedium,
                 )
               : const SizedBox()),
           Container(
